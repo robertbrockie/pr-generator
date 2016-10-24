c549989046f7cffc6edb22a3fdbc2db65ef936e2
Avi Aialon <aviaialon@gmail.com>
2016-10-24 09:15:33 -0400

    copy to clipboard

--- chrome_extension/main.js
+++ chrome_extension/main.js
@@ -3,6 +3,13 @@ function extractPullRequestDescription() {
 }
 
 if (window.location.host.indexOf("github.com") > -1) {
-	alert(">*DEPLOYING* `" + window.location.origin + window.location.pathname + 
-			"` _" + extractPullRequestDescription() + "_");
+	// Create a "hidden" input
+	var aux = document.createElement("input");
+
+	aux.setAttribute("value", ">*DEPLOYING* `" + window.location.origin + window.location.pathname + "` _" + extractPullRequestDescription() + "_");
+	document.body.appendChild(aux);
+	aux.select();
+	document.execCommand("copy");
+	document.body.removeChild(aux);
+	alert('copied to clipboard')
 }
