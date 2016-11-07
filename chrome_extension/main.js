6c8b429785ed1390eb1e6fa377ebe9a2fa641a88
Robert Brockie <robertbrockie@gmail.com>
2016-11-06 23:21:23 -0500

    Added some humor to the project.

--- chrome_extension/main.js
+++ chrome_extension/main.js
@@ -1,12 +1,22 @@
-var message = document.querySelector('#message');
-
 chrome.runtime.onMessage.addListener(function(request, sender) {
 	var message = document.querySelector('#message');
+	var loading = document.querySelector('#loading');
+	var success = document.querySelector('#success');
+	var error   = document.querySelector('#error');
 
 	if (request.action == "getSource") {
 		if (request.source) {
-			message.innerText = request.source;
-			// Copy and paste the text
+			
+			// hide the loading image
+			loading.style.display = 'none';
+
+			// show success image
+			success.style.display = 'block'
+
+			// render the success message
+			message.innerText = "Go deploy that shit!\n\nDeploy message pasted and ready for Slack";
+
+			// copy the Slack message to the clipboard
 			var aux = document.createElement("input");
 			aux.setAttribute("value", request.source);
 			document.body.appendChild(aux);
@@ -14,7 +24,14 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
 			document.execCommand("copy");
 			document.body.removeChild(aux);
 		} else {
-			message.innerText = "Get a Pull Request going homie!";
+
+			// hide the loading image
+			loading.style.display = 'none';
+
+			// show success image
+			error.style.display = 'block'
+			
+			message.innerText = "This is no Pull Request, get to work!";
 		}
 	}
 });
@@ -23,6 +40,8 @@ function onWindowLoad() {
 	chrome.tabs.executeScript(null, {
 		file: "extractData.js"
 	}, function() {
+		var message = document.querySelector('#message');
+
 		// If you try and inject into an extensions page or the webstore/NTP you'll get an error
 		if (chrome.runtime.lastError) {
 			message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
